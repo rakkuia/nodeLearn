9d4cf88c7e0a7a9c6cfe93c179878da3ff8024a2
R <rodrigoraccuia@unesc.net>
2022-05-10 14:57:50 -0300

    playing console

---
+++ 2_MODULES/7_playing_console/index.js
@@ -0,0 +1,20 @@
+const x = 10;
+const y = 'LOREM IPSUM';
+const z = [1, 2];
+
+console.log(x, y, z);
+
+//Console counter
+console.count(`The value of X is ${x}, count`);
+console.count(`The value of X is ${x}, count`);
+console.count(`The value of X is ${x}, count`);
+console.count(`The value of X is ${x}, count`);
+
+//var w string
+console.log("The name is %s", y);
+
+//clean console
+setTimeout(() =>{
+console.clear();
+
+}, 2000);
