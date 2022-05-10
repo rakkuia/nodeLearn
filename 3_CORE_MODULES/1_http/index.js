ff6e1db10e29180dbb80dfe9a9da9aacae1e3379
R <rodrigoraccuia@unesc.net>
2022-05-10 16:48:35 -0300

    http

---
+++ 3_CORE_MODULES/1_http/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+    res.write('Hi HTTP');
+    res.end();
+
+});
+
+server.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
