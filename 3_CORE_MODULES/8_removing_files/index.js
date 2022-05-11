14f8528e68fd76c9b860fc4e5a3d85025d56790f
R <rodrigoraccuia@unesc.net>
2022-05-11 14:48:38 -0300

    removing files

---
+++ 3_CORE_MODULES/8_removing_files/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+fs.unlink('file.txt', function (err) {
+
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log('File Removed');
+
+})
