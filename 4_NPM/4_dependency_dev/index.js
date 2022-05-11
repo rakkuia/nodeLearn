e9d1ede58c002dd6021da04508e544cdf367c153
R <rodrigoraccuia@unesc.net>
2022-05-11 17:28:41 -0300

    dev dependencies

---
+++ 4_NPM/4_dependency_dev/index.js
@@ -0,0 +1,9 @@
+const _ = require('lodash');
+const penis = require('chalk');
+
+const a = [1, 2, 3];
+const b = [1, 39, 4, 5, 6];
+
+const diff = _.difference(a, b);
+
+console.log(penis.blue.bold("yo yo yo Mr"), "wÆltar");
