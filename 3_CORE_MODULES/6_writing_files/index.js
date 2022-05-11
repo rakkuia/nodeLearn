f0342e47822bbabbcf74a5947e2ed5d22c30d461
R <rodrigoraccuia@unesc.net>
2022-05-11 14:18:25 -0300

    lol

---
+++ 3_CORE_MODULES/6_writing_files/index.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+    const urlInfo = require('url').parse(req.url, true);
+    const name = urlInfo.query.name;
+
+
+if(!name){
+    fs.readFile('file.html', function(err, data) {
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write(data);
+        return res.end();
+       });
+    }
+
+    
+    else{
+        fs.writeFile(`${name}${i}.txt`, name,  function(err, data) {
+            res.writeHead(302, {
+                Location:'/',
+            })
+                return res.end(); 
+        })
+    }
+}
+  
+ 
+});
+
+server.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
