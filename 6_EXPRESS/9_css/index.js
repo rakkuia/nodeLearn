e456b601e40d41206da1e14b59ff7a76cf7a0cd2
R <rodrigoraccuia@unesc.net>
2022-05-13 16:08:28 -0300

    css

---
+++ 6_EXPRESS/9_css/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const path = require("path");
+const users = require('./users');
+
+//read Body
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
+app.use(express.json());
+
+//static files
+app.use(express.static('public'));
+
+
+
+const basePath = path.join(__dirname, 'templates');
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+})
+
