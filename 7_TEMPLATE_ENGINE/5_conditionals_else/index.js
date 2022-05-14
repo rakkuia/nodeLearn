b1cc3b77e09e416296f286621b7fe8af6f698db5
R <rodrigoraccuia@unesc.net>
2022-05-13 21:26:29 -0300

    else

---
+++ 7_TEMPLATE_ENGINE/5_conditionals_else/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+
+
+})
+
+app.get('/', (req, res) => {
+    const user ={
+        name: "r",
+        surname: "rakk"
+    }
+
+    const word = 'word';
+    
+    const auth = true;
+
+    const approved = false;
+
+    res.render('home', {user: user, word, auth, approved});
+})
+
+app.listen(3000, () =>{ 
+    console.log('App initialized');
+})
